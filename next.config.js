04009b91b40edd8259832fa3ccc70b76c78f5af3
Ahmed Mohammed <AhmedIsmail73990@azhar.edu.eg>
2025-05-31 14:55:36 +0300

    fix: Update API_URL and add additional remote image patterns for Sayyn services

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: 'https://test.fixawi.com/api/v1'
+        API_URL: 'https://api.sayyn.net/api/v1'
     },
     images: {
         remotePatterns: [
@@ -29,6 +29,34 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'localhost'
+            },
+            {
+                protocol: 'http',
+                hostname: 'api.sayyn.net'
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.sayyn.net'
+            },
+            {
+                protocol: 'https',
+                hostname: 'sayyn.net'
+            },
+            {
+                protocol: 'https',
+                hostname: 'sayyn.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'sayyn.org'
+            },
+            {
+                protocol: 'https',
+                hostname: 'sayyn.app'
+            },
+            {
+                protocol: 'https',
+                hostname: 'sayyn.io'
             }
         ]
     }
